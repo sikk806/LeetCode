a7df25041e82baa1347606541e591b6da5e68682
sikk806 <54883267+sikk806@users.noreply.github.com>
2024-09-02 11:56:07 +0900

    Time: 0 ms (100%), Space: 10.8 MB (34.26%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,11 +3,20 @@ public:
     int removeElement(vector<int>& nums, int val) {
         vector<int> answer;
         int count = 0;
-        for(int& i : nums)
+        // for(int& i : nums)
+        // {
+        //     if(i != val)
+        //     {
+        //         answer.push_back(i);
+        //         count++;
+        //     }
+        // }
+
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(i != val)
+            if(nums[i] != val)
             {
-                answer.push_back(i);
+                answer.push_back(nums[i]);
                 count++;
             }
         }
