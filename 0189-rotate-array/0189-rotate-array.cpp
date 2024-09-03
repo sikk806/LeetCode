ff40a21cafffcf46468be523edb77e6968610703
sikk806 <54883267+sikk806@users.noreply.github.com>
2024-09-03 12:10:47 +0900

    Time: 44 ms (6.22%), Space: 29 MB (5.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,6 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         vector<int> copy;
         int size = nums.size();
+        k %= nums.size();
         for(int i = size - k; i < size; i++)
         {
             cout << nums[i] << " ";
