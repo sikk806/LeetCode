695267825e81e7a412aa2a3b23bebc62341b0c0d
sikk806 <54883267+sikk806@users.noreply.github.com>
2024-09-06 11:17:32 +0900

    Time: 22 ms (43.58%), Space: 29 MB (5.41%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,12 +6,10 @@ public:
         k %= nums.size();
         for(int i = size - k; i < size; i++)
         {
-            cout << nums[i] << " ";
             copy.push_back(nums[i]);
         }
         for(int i = 0; i < size-k; i++)
         {
-            cout << nums[i] << " ";
             copy.push_back(nums[i]);
         }
         nums = copy;
